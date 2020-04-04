0a5aa8dbd4d600c1f72e405bb8aefe57b63bd612
quimicaproduce <ivancalvo91@gmail.com>
2020-04-04 10:16:45 -0500

    locsal hosting

---
+++ nodePractices/firstApp/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+let app = new express();
+
+app.get("/", (req,res)=>{
+    res.send("hello, from node example");
+});
+
+app.get("/about", (req, res)=>{
+    res.send("About this web App");
+});
+
+app.get("/contact", (req, res)=>{
+    res.send("contact us at the address below");
+});
+// listen to the server!!!!!
+let port = 5000
+app.listen(5000, ()=>{
+    console.log("Server is listening to: " + port);
+});
